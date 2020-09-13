281493e59c0610e341c108e0d8f8a31ec6ec43c0
Kizomba <kizomba@github.com>
2020-09-13 10:48:56 +0200

    1

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ import time
 ###################
 st.title('My first app')
 st.write("Here's our first attempt at using data to create a table:")
+'''
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
@@ -26,3 +27,4 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 st.line_chart(chart_data)
 ###################
+'''
